2eaa9069881fdcf99db3a88bebac9132ef108bfa
Francisco Augusto Guiraldelli <francisco.augusto@clear.sale>
2023-03-24 20:48:26 +0100

    First commit create event-driven microservices using RabbitMQ

---
+++ order-service/src/test/java/fguiraldelli/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fguiraldelli.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ stock-service/src/main/java/fguiraldelli/stockservice/dto/Order.java
@@ -0,0 +1,18 @@
+package fguiraldelli.stockservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+    private String orderId;
+    private String name;
+    private int qty;
+    private BigDecimal price;
+}
+
